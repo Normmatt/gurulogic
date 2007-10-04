aa32d79974f3c7ed172dbfb086b5a2fc57524601
normmatt234 <normmatt234@9f22a48f-a63b-0410-aaaf-636bd12ddef9>
2007-10-04 09:23:04 +0000

    Removed file/folder

--- level.h
+++
@@ -1,9 +0,0 @@
-#ifndef LEVEL_H
-#define LEVEL_H
-
-void ReadMapFile(char *filename);
-void LoadTileset(char *filename);
-void FreeTileset();
-void DrawLevel(char* map_name, char* tileset_name);
-
-#endif
