957624a27451a050bf77fd5c525c46cbc96d93d5
normmatt234 <normmatt234@9f22a48f-a63b-0410-aaaf-636bd12ddef9>
2010-01-01 14:13:32 +0000

    latest code

--- GuruLogic/level.h
+++ GuruLogic/level.h
@@ -1,11 +1,23 @@
 #ifndef LEVEL_H
 #define LEVEL_H
 
+#define TYPE_BLANK      0
+#define TYPE_WHITE      1
+#define TYPE_WALL       2
+#define TYPE_BLUE       3
+#define TYPE_BLUEROLL   4
+#define TYPE_BOUNCE     5
+#define TYPE_HOLE       6
+#define TYPE_RED        7
+#define TYPE_REDROLL    8
+
 void ReadMapFile(char *filename);
 void LoadTileset(char *filename);
 void FreeTileset();
 void InitLevel(char* map_name, char* tileset_name);
+
 void DrawLevel();
+void DrawMinimap();
 
 void RotateRight90();
 void RotateLeft90();
@@ -16,14 +28,5 @@ void DrawAimTail();
 int GetBlockCount();
 void ShootBlock();
 void RetractBlock();
-
-typedef struct
-{
-   unsigned char rotated_left;
-   unsigned char rotated_right;
-}
-gBoard;
-
-extern gBoard Board;
 
 #endif
